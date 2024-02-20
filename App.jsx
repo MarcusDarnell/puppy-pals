1e10e8575008acc3ad5a76852298487b36edfb1f
MarcusDarnell <mdarnell12894@gmail.com>
2024-02-19 20:55:08 -0800

    first commit

--- App.jsx
+++ App.jsx
@@ -3,14 +3,18 @@ import {useState} from 'react'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log('puppyList: ', puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+function handleClick() {
+    onClick = {() => {console.log('puppy id', puppy.id)}};
+}
+  
   return (
-  <div className="App">
-      { 
-   puppies.map((puppy) => {
-     return <p key={puppy.id}>{puppy.name}</p>;
-   })
-  }
+  <div className="App">      
+   {puppies.map((puppy) => {
+     return ( <p onClick={handleClick} key={puppy.id}>{puppy.name}</p>
+   );
+  })}
 </div>
   );
 }
